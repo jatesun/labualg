719a4cbdc59b568e8380b8765299216dac1a54f7
jatesun <jatesun111@gmail.com>
2025-01-14 19:34:20 +0800

    2025-01-08 leecode 23 mergeKLists_23

---
+++ alg/src/main/java/zero/twopointer/MergeKLists_23.java
@@ -0,0 +1,89 @@
+package zero.twopointer;
+
+import base.ListNode;
+
+import java.util.PriorityQueue;
+
+/**
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * <p>
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ * 示例 2：
+ * <p>
+ * 输入：lists = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：lists = [[]]
+ * 输出：[]
+ */
+public class MergeKLists_23 {
+    /**
+     * k个链表合并，原理其实跟双链表相同。只不是变成了k个。我们可以设置k个节点
+     * 1.设置k个节点，分别对应链表k个。
+     * 2.获取最小的节点，赋值到结果中，当前节点后移动,如果后面没有元素了，就从数组中删除。(其实为优先队列)
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        // 优先级队列，最小堆
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+                lists.length, (a, b) -> (a.val - b.val));
+        // 将 k 个链表的头结点加入最小堆
+        for (ListNode head : lists) {
+            if (head != null) {
+                pq.add(head);
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            // 获取最小节点，接到结果链表中
+            ListNode node = pq.poll();
+            p.next = node;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+        return dummy.next;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
